d2127a7e8d77b25be5e7b9adcbbda555d7d3cb0b
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2024-05-22 17:30:47 +0800

    feat: add rollup-plugin-copy
    This commit introduces the rollup-plugin-copy to the project. The plugin
    allows copying files and directories during the build process,
    facilitating tasks such as copying assets or static files to the build
    output directory. This addition enhances the build pipeline and enables
    more flexible handling of project assets.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,28 @@
-import css from "rollup-plugin-import-css"
+import css from 'rollup-plugin-import-css'
+import copy from 'rollup-plugin-copy'
 
 export default {
-	input: "./src/index.js",
+	input: './src/index.js',
 	output: {
-		file: "./dist/bundle.js",
-		format: "cjs",
+		file: './dist/bundle.js',
+		format: 'cjs',
 	},
-	plugins: [css()],
+	plugins: [
+		css(),
+		copy({
+			targets: [
+				{ src: './src/index.html', dest: './dist/index.html' },
+				{
+					src: [
+						'./src/assets/fonts/Quicksand-Bold.ttf',
+						'./src/assets/fonts/Quicksand-Regular.ttf',
+						'./src/assets/fonts/Ravie Regular.ttf',
+					],
+					dest: './dist/assets/fonts',
+				},
+				{ src: './src/assets/images/*', dest: './dist/assets/images' },
+				{ src: './src/assets/favicons/*', dest: './dist/assets/favicons' },
+			],
+		}),
+	],
 }
